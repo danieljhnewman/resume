a0358c89f24465a2b9d956c1750bc2e9013dc86a
Daniel Newman <danieljhnewman@gmail.com>
2024-03-30 21:00:44 -0700

    First commit

---
+++ counter.js
@@ -0,0 +1,15 @@
+fetch("https://xzsh4u3rj4ykvfa6jzcmispycu0mncqb.lambda-url.us-east-1.on.aws/").then( function(response) {
+    return response.json()
+    //✅data types:
+    //1. json 
+    //2. text 
+    //3. blob 
+    
+}).then(function(result){
+    //✅when promise is returned succesfully, response.data is logged inside result variable 
+    document.getElementById("hitReturn").innerHTML = result.Item.hitcount.N;
+    
+}).catch(function(error){
+    //✅when promise is failed 
+    console.log(error)
+})
